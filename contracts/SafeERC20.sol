60bd7b075df9949a7926e58fa76a6aa97379f8db
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2022-12-11 16:18:43 +0700

    introduce SafeERC20.sol

---
+++ contracts/SafeERC20.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: -- BCOM --
+
+pragma solidity =0.8.17;
+
+import "./IERC20.sol";
+
+contract SafeERC20 {
+
+    function safeTransfer(
+        IERC20 _token,
+        address _to,
+        uint256 _value
+    )
+        internal
+    {
+        callOptionalReturn(
+            _token,
+            abi.encodeWithSelector(
+                _token.transfer.selector,
+                _to,
+                _value
+            )
+        );
+    }
+
+    function safeTransferFrom(
+        IERC20 _token,
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        internal
+    {
+        callOptionalReturn(
+            _token,
+            abi.encodeWithSelector(
+                _token.transferFrom.selector,
+                _from,
+                _to,
+                _value
+            )
+        );
+    }
+
+    function callOptionalReturn(
+        IERC20 _token,
+        bytes memory _data
+    )
+        private
+    {
+        (
+            bool success,
+            bytes memory returndata
+        ) = address(_token).call(_data);
+
+        require(
+            success,
+            "SafeERC20: CALL_FAILED"
+        );
+
+        if (returndata.length > 0) {
+            require(
+                abi.decode(returndata, (bool)),
+                "SafeERC20: OPERATION_FAILED"
+            );
+        }
+    }
+}
