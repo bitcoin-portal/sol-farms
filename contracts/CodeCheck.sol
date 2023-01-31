61fab68a61575844164a7f6f3cb5a5c79c3a63c4
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-01-31 14:28:22 +0700

    expected hash and codeChecker added

---
+++ contracts/CodeCheck.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: --BCOM--
+
+pragma solidity =0.8.17;
+
+import "./SimpleFarm.sol";
+
+contract FarmCodeCheck {
+
+    function farmCodeHash()
+        external
+        pure
+        returns (bytes32)
+    {
+        return keccak256(
+            type(SimpleFarm).creationCode
+        );
+    }
+}
