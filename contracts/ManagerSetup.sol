6d5ba64c38e1cdc833cdccfb327337fb3a39f60a
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2024-01-14 02:11:21 +0700

    add allocations

--- contracts/ManagerSetup.sol
+++ contracts/ManagerSetup.sol
@@ -95,8 +95,8 @@ contract ManagerSetup {
 
         allocations.push(
             Allocation({
-                unlock20Percent: true,
-                stakeOwner: 0x08A39aE0b0dA06fE824a65fA0A73C3126A82A0bA,
+                unlock20Percent: false,
+                stakeOwner: 0x01b7b155f4934d52AAb625E8F7f20F81f78211Aa,
                 stakeAmount: 56_250_000,
                 vestingTime: fourYears
             })
@@ -105,7 +105,7 @@ contract ManagerSetup {
         allocations.push(
             Allocation({
                 unlock20Percent: true,
-                stakeOwner: 0xB413C4cF16A50E45D4101380AA0EDC1859Aa0a03,
+                stakeOwner: 0x0CefAbb19e909Dee95CFa1F007a20AFdcF9020d1,
                 stakeAmount: 50_000_000,
                 vestingTime: fourYears
             })
@@ -114,7 +114,7 @@ contract ManagerSetup {
         allocations.push(
             Allocation({
                 unlock20Percent: true,
-                stakeOwner: 0xA02351E83625c5185908835846B26719Fcd3d53F,
+                stakeOwner: 0x104aA2d7195Ce3f445c9B675b80960181e062357,
                 stakeAmount: 37_500_000,
                 vestingTime: fourYears
             })
@@ -123,7 +123,7 @@ contract ManagerSetup {
         allocations.push(
             Allocation({
                 unlock20Percent: true,
-                stakeOwner: 0x641AD78BAca220C5BD28b51Ce8e0F495e85Fe689,
+                stakeOwner: 0x12357c21d95eB08E2D9C9262Edf6e09306de801d,
                 stakeAmount: 31_250_000,
                 vestingTime: fourYears
             })
@@ -132,7 +132,7 @@ contract ManagerSetup {
         allocations.push(
             Allocation({
                 unlock20Percent: true,
-                stakeOwner: 0xE482Bd6f975fF1dda8f2FD375CA193DaCD38235a,
+                stakeOwner: 0x13879cB7382c64Fa84FA4ecdE79dc8ae8EEA3E2e,
                 stakeAmount: 25_000_000,
                 vestingTime: fourYears
             })
@@ -141,7 +141,7 @@ contract ManagerSetup {
         allocations.push(
             Allocation({
                 unlock20Percent: true,
-                stakeOwner: 0x717d52e84eF30875De52834603c112675CeDB7CA,
+                stakeOwner: 0x181f392eb50bD52d03aA473982eDd568d449b20A,
                 stakeAmount: 18_750_000,
                 vestingTime: fourYears
             })
@@ -150,7 +150,133 @@ contract ManagerSetup {
         allocations.push(
             Allocation({
                 unlock20Percent: true,
-                stakeOwner: 0xa803c226c8281550454523191375695928DcFE92,
+                stakeOwner: 0x1B006a0318dF1cBcC072D1505F641796E0EA59B3,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x1C5Cb9daFaBd8CBA532e52eF30A3C14cCCba06E8,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x2062C4515f7DF5B0D71A3450e5cD5574b507ba3a,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x216b6F99CA2bf53d801fE9Ba7d68fADC4949249B,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x21F107026a0f746b81Fb0A47A1314f4AB4390B63,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x2d75e5D15837D780e751e2d4133cF59Dca281200,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x3b28c1fF09c72915A1bd44C8A5c8C9018159b4C2,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x3cee2c3869e5477FC56eCb2009167899415D7fBF,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x44a5eeE514A39B0c2e887A5a403170d84C563423,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x50319e5033010395C7da40f27C37Ed4BB2E92d6D,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x566CAd1FBA28aC265f046579261695D4c9F6d4da,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x568CE9c5a37E434a5521E3a8230b8D357cE89330,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x5f21e45CED3C5a8d4F6dCd0550a5541eaa15f36E,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x636EE9dd1a3F9b85A78b8c08c1E5A8506703fc8A,
+                stakeAmount: 12_500_000,
+                vestingTime: fourYears
+            })
+        );
+
+        allocations.push(
+            Allocation({
+                unlock20Percent: true,
+                stakeOwner: 0x641AD78BAca220C5BD28b51Ce8e0F495e85Fe689,
                 stakeAmount: 12_500_000,
                 vestingTime: fourYears
             })
