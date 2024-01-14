3a6bdb8bfaa2453769b9d58ebbfa75e6027a6c51
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2024-01-15 01:07:14 +0700

    add foundry test file

---
+++ contracts/TimeLockFarmV2Dual.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: -- WISE --
+
+pragma solidity =0.8.23;
+
+import "forge-std/Test.sol";
+import "./TimeLockFarmV2Dual.sol";
+
+contract TimeLockFarmV2DualTest is Test {
+
+    TimeLockFarmV2Dual public farm;
+
+    IERC20 USDC_TOKEN = IERC20(
+        0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+    );
+
+    IERC20 VERSE_TOKEN = IERC20(
+        0x249cA82617eC3DfB2589c4c17ab7EC9765350a18
+    );
+
+    uint256 constant FORK_MAINNET_BLOCK = 18_704_404;
+
+    address constant ADMIN_ADDRESS = address(
+        0x641AD78BAca220C5BD28b51Ce8e0F495e85Fe689
+    );
+
+    uint256 public constant DEFAULT_DURATION = 30 days;
+
+    function setUp()
+        public
+    {
+        vm.createSelectFork(
+            vm.rpcUrl("mainnet"),
+            FORK_MAINNET_BLOCK
+        );
+
+        vm.startPrank(
+            ADMIN_ADDRESS
+        );
+
+        farm = new TimeLockFarmV2Dual({
+            _stakeToken: VERSE_TOKEN,
+            _rewardTokenA: VERSE_TOKEN,
+            _rewardTokenB: USDC_TOKEN,
+            _defaultDuration: DEFAULT_DURATION
+        });
+
+        VERSE_TOKEN.approve(
+            address(farm),
+            type(uint256).max
+        );
+
+        USDC_TOKEN.approve(
+            address(farm),
+            type(uint256).max
+        );
+    }
+
+    function testChangeDuration()
+        public
+    {
+        uint256 expectedDuration = DEFAULT_DURATION;
+        uint256 updatedDuration = 60 days;
+
+        uint256 duration = farm.rewardDuration();
+
+        assertEq(
+            duration,
+            expectedDuration
+        );
+
+        farm.setRewardDuration(
+            updatedDuration
+        );
+
+        duration = farm.rewardDuration();
+
+        assertEq(
+            duration,
+            updatedDuration
+        );
+    }
+}
