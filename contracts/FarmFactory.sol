9cd67d39ce92bbea886c0926e1fccc0dd6c4b32a
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2024-09-17 00:59:41 +0700

    use ISimpleFarm

--- contracts/FarmFactory.sol
+++ contracts/FarmFactory.sol
@@ -2,14 +2,16 @@
 
 pragma solidity =0.8.25;
 
-interface SimpleFarm {
+interface ISimpleFarm {
 
     function initialize(
         address _stakeToken,
         address _rewardToken,
         uint256 _defaultDuration,
         address _owner,
-        address _manager
+        address _manager,
+        string calldata _name,
+        string calldata _symbol
     )
         external;
 }
@@ -36,7 +38,9 @@ contract FarmFactory {
     function createFarm(
         address _stakeToken,
         address _rewardToken,
-        uint256 _defaultDuration
+        uint256 _defaultDuration,
+        string  calldata _farmTokenName,
+        string  calldata _farmTokenSymbol
     )
         external
         returns (address)
@@ -44,14 +48,18 @@ contract FarmFactory {
         return _clone(
             _stakeToken,
             _rewardToken,
-            _defaultDuration
+            _defaultDuration,
+            _farmTokenName,
+            _farmTokenSymbol
         );
     }
 
     function _clone(
         address _stakeToken,
         address _rewardToken,
-        uint256 _defaultDuration
+        uint256 _defaultDuration,
+        string  calldata _farmTokenName,
+        string  calldata _farmTokenSymbol
     )
         private
         returns (address simpleFarm)
@@ -96,12 +104,14 @@ contract FarmFactory {
             )
         }
 
-        SimpleFarm(simpleFarm).initialize(
+        ISimpleFarm(simpleFarm).initialize(
             _stakeToken,
             _rewardToken,
             _defaultDuration,
             msg.sender,
-            msg.sender
+            msg.sender,
+            _farmTokenName,
+            _farmTokenSymbol
         );
 
         emit FarmCreated(
