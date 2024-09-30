e9328205a61333aa1922c70e38947c67f9af0758
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2024-09-30 20:51:38 +0200

    DynamicRewardFarm added

--- contracts/DynamicRewardFarm.sol
+++ contracts/DynamicRewardFarm.sol
@@ -4,6 +4,11 @@ pragma solidity =0.8.26;
 
 import "./TokenWrapper.sol";
 
+error NoRewards();
+error UnknownToken();
+error ExistingToken();
+error InvalidAddress();
+
 contract DynamicRewardFarm is TokenWrapper {
 
     IERC20 public stakeToken;
@@ -12,14 +17,19 @@ contract DynamicRewardFarm is TokenWrapper {
     uint256 public rewardDuration;
     uint256 public lastUpdateTime;
 
-    uint256 constant PRECISION = 1E18;
+    uint256 constant MAX_TOKENS = 10;
+    uint256 constant PRECISIONS = 1E18;
 
     address public ownerAddress;
     address public proposedOwner;
     address public managerAddress;
 
-    IERC20[] public rewardTokens;
     uint256 public tokenCount;
+    address[] public rewardTokens;
+
+    address constant DEAD_ADDRESS = address(
+        0x000000000000000000000000000000000000dEaD
+    );
 
     struct RewardData {
         uint256 rewardRate;
@@ -47,112 +57,19 @@ contract DynamicRewardFarm is TokenWrapper {
     }
 
     modifier updateFarm() {
-        for (uint256 i = 0; i < rewardTokens.length; i++) {
-            IERC20 rewardToken = rewardTokens[i];
-            rewards[address(rewardToken)].perTokenStored = rewardPerToken(
-                rewardToken
-            );
-        }
-        lastUpdateTime = lastTimeRewardApplicable();
+        _updateFarm();
         _;
     }
 
     modifier updateUser() {
-        for (uint256 i = 0; i < rewardTokens.length; i++) {
-            IERC20 rewardToken = rewardTokens[i];
-            address tokenAddress = address(rewardToken);
-
-            RewardData storage r = rewards[
-                tokenAddress
-            ];
-
-            // Initialize perTokenPaid for new reward tokens
-            if (r.perTokenPaid[msg.sender] == 0 && _balances[msg.sender] > 0) {
-                r.perTokenPaid[msg.sender] = r.perTokenStored;
-            }
-
-            r.userRewards[msg.sender] = earnedByToken(
-                rewardToken,
-                msg.sender
-            );
-
-            r.perTokenPaid[msg.sender] = r.perTokenStored;
-        }
+        _updateUser(msg.sender);
         _;
     }
 
-    modifier updateSender(
-        address _sender
-    ) {
-        for (uint256 i = 0; i < rewardTokens.length; i++) {
-
-            IERC20 rewardToken = rewardTokens[i];
-
-            address tokenAddress = address(
-                rewardToken
-            );
-
-            RewardData storage r = rewards[
-                tokenAddress
-            ];
-
-            // Initialize perTokenPaid for new reward tokens
-            if (r.perTokenPaid[_sender] == 0 && _balances[_sender] > 0) {
-                r.perTokenPaid[_sender] = r.perTokenStored;
-            }
-
-            r.userRewards[_sender] = earnedByToken(
-                rewardToken,
-                _sender
-            );
-
-            r.perTokenPaid[_sender] = r.perTokenStored;
-        }
+    modifier updateSender(address _sender) {
+        _updateUser(_sender);
         _;
     }
-
-    event Staked(
-        address indexed user,
-        uint256 tokenAmount
-    );
-
-    event Withdrawn(
-        address indexed user,
-        uint256 tokenAmount
-    );
-
-    event RewardAdded(
-        address indexed rewardToken,
-        uint256 rewardRate,
-        uint256 tokenAmount
-    );
-
-    event RewardPaid(
-        address indexed user,
-        address indexed rewardToken,
-        uint256 tokenAmount
-    );
-
-    event Recovered(
-        IERC20 indexed token,
-        uint256 tokenAmount
-    );
-
-    event RewardsDurationUpdated(
-        uint256 newRewardDuration
-    );
-
-    event OwnerProposed(
-        address proposedOwner
-    );
-
-    event OwnerChanged(
-        address newOwner
-    );
-
-    event ManagerChanged(
-        address newManager
-    );
 
     function initialize(
         address _stakeToken,
@@ -185,6 +102,11 @@ contract DynamicRewardFarm is TokenWrapper {
 
         ownerAddress = _ownerAddress;
         managerAddress = _managerAddress;
+
+        _stake(
+            PRECISIONS,
+            DEAD_ADDRESS
+        );
     }
 
     /**
@@ -194,24 +116,58 @@ contract DynamicRewardFarm is TokenWrapper {
         address _rewardToken
     )
         external
-        onlyManager
+        onlyOwner
     {
-        if (_rewardToken == ZERO_ADDRESS) {
-            revert("DynamicRewardFarm: WRONG_ADDRESS");
-        }
-
-        // Ensure the reward token is not yet added
-        for (uint256 i = 0; i < rewardTokens.length; i++) {
-            if (address(rewardTokens[i]) == _rewardToken) {
-                revert("DynamicRewardFarm: TOKEN_ALREADY_ADDED");
-            }
-        }
-
-        rewardTokens.push(
-            IERC20(_rewardToken)
+        require(
+            tokenCount < MAX_TOKENS,
+            "DynamicRewardFarm: MAX_TOKENS_REACHED"
         );
 
-        tokenCount++;
+        _validateRewardToken(
+            _rewardToken
+        );
+
+        rewardTokens.push(
+            _rewardToken
+        );
+
+        tokenCount = tokenCount + 1;
+    }
+
+    /**
+     * @dev Checks if the token is already added
+     */
+    function _validateRewardToken(
+        address _tokenAddress
+    )
+        private
+        view
+    {
+        if (_tokenAddress == ZERO_ADDRESS) {
+            revert InvalidAddress();
+        }
+
+        for (uint256 i; i < rewardTokens.length; i++) {
+            if (_tokenAddress == rewardTokens[i]) {
+                revert ExistingToken();
+            }
+        }
+    }
+
+    function getRewardTokens()
+        external
+        view
+        returns (address[] memory)
+    {
+        address[] memory tokens = new address[](
+            rewardTokens.length
+        );
+
+        for (uint256 i; i < rewardTokens.length; i++) {
+            tokens[i] = rewardTokens[i];
+        }
+
+        return tokens;
     }
 
     /**
@@ -228,36 +184,44 @@ contract DynamicRewardFarm is TokenWrapper {
     }
 
     /**
-     * @dev Relative value on reward for single staked token for a given reward token
+     * @dev Relative value on reward for single
+     * staked token for a given {_rewardToken}
      */
     function rewardPerToken(
-        IERC20 _rewardToken
+        address _rewardToken
     )
         public
         view
         returns (uint256)
     {
         if (_totalStaked == 0) {
-            return rewards[address(_rewardToken)].perTokenStored;
+            return rewards[_rewardToken].perTokenStored;
         }
+
+        RewardData storage r = rewards[
+            _rewardToken
+        ];
 
         uint256 timeFrame = lastTimeRewardApplicable()
             - lastUpdateTime;
 
         uint256 extraFund = timeFrame
-            * rewards[address(_rewardToken)].rewardRate
-            * PRECISION
+            * r.rewardRate
+            * PRECISIONS
             / _totalStaked;
 
-        return rewards[address(_rewardToken)].perTokenStored
+        return r.perTokenStored
             + extraFund;
     }
 
     /**
-     * @dev Returns an array of earned amounts for all reward tokens for the caller
+     * @dev Returns an array of earned amounts for
+     * all reward tokens by given {_walletAddress}
      */
-    function earned()
-        public
+    function earned(
+        address _walletAddress
+    )
+        external
         view
         returns (uint256[] memory)
     {
@@ -265,10 +229,10 @@ contract DynamicRewardFarm is TokenWrapper {
             rewardTokens.length
         );
 
-        for (uint256 i = 0; i < rewardTokens.length; i++) {
+        for (uint256 i; i < rewardTokens.length; i++) {
             earnedAmounts[i] = earnedByToken(
                 rewardTokens[i],
-                msg.sender
+                _walletAddress
             );
         }
 
@@ -276,10 +240,11 @@ contract DynamicRewardFarm is TokenWrapper {
     }
 
     /**
-     * @dev Reports earned amount of a reward token by wallet address not yet collected
+     * @dev Reports earned amount of a reward
+     * token by wallet address not yet collected
      */
     function earnedByToken(
-        IERC20 _rewardToken,
+        address _rewardToken,
         address _walletAddress
     )
         public
@@ -287,25 +252,15 @@ contract DynamicRewardFarm is TokenWrapper {
         returns (uint256)
     {
         RewardData storage r = rewards[
-            address(_rewardToken)
+            _rewardToken
         ];
-
-        uint256 perTokenPaidValue = r.perTokenPaid[
-            _walletAddress
-        ];
-
-        // If perTokenPaid is zero and user has a balance,
-        // assume they haven't started earning this reward
-        if (perTokenPaidValue == 0 && _balances[_walletAddress] > 0) {
-            return r.userRewards[_walletAddress];
-        }
 
         uint256 difference = rewardPerToken(_rewardToken)
-            - perTokenPaidValue;
+            - r.perTokenPaid[_walletAddress];
 
         return _balances[_walletAddress]
             * difference
-            / PRECISION
+            / PRECISIONS
             + r.userRewards[_walletAddress];
     }
 
@@ -346,12 +301,6 @@ contract DynamicRewardFarm is TokenWrapper {
         updateFarm()
         updateUser()
     {
-        require(
-            _totalStaked > _withdrawAmount ||
-            block.timestamp > periodFinished,
-            "DynamicRewardFarm: STILL_EARNING"
-        );
-
         address senderAddress = msg.sender;
 
         _withdraw(
@@ -392,27 +341,25 @@ contract DynamicRewardFarm is TokenWrapper {
     {
         address senderAddress = msg.sender;
 
-        for (uint256 i = 0; i < rewardTokens.length; i++) {
+        for (uint256 i; i < rewardTokens.length; i++) {
 
-            IERC20 rewardToken = rewardTokens[i];
-
-            address tokenAddress = address(
-                rewardToken
-            );
+            address tokenAddress = rewardTokens[i];
 
             RewardData storage r = rewards[
                 tokenAddress
             ];
 
-            uint256 rewardAmount = r.userRewards[
+            uint256 rewardAmount = earnedByToken(
+                tokenAddress,
                 senderAddress
-            ];
+            );
 
             if (rewardAmount > 0) {
+
                 r.userRewards[senderAddress] = 0;
 
                 safeTransfer(
-                    rewardToken,
+                    IERC20(tokenAddress),
                     senderAddress,
                     rewardAmount
                 );
@@ -465,7 +412,7 @@ contract DynamicRewardFarm is TokenWrapper {
         onlyOwner
     {
         if (_newManager == ZERO_ADDRESS) {
-            revert("DynamicRewardFarm: WRONG_ADDRESS");
+            revert("DynamicRewardFarm: ZERO_ADDRESS");
         }
 
         managerAddress = _newManager;
@@ -476,31 +423,51 @@ contract DynamicRewardFarm is TokenWrapper {
     }
 
     function recoverToken(
-        IERC20 _tokenAddress,
-        uint256 _tokenAmount
+        address _tokenAddress,
+        uint256 _recoveryAmount
     )
         external
         onlyOwner
     {
-        if (address(_tokenAddress) == ZERO_ADDRESS) {
-            revert("DynamicRewardFarm: WRONG_ADDRESS");
+        IERC20 tokenAddress = IERC20(
+            _tokenAddress
+        );
+
+        if (tokenAddress == stakeToken) {
+            revert("DynamicRewardFarm: STAKE_TOKEN");
         }
 
-        for (uint256 i = 0; i < rewardTokens.length; i++) {
-            if (address(_tokenAddress) == address(rewardTokens[i])) {
-                revert("DynamicRewardFarm: REWARD_TOKEN");
+        for (uint256 i; i < rewardTokens.length; i++) {
+            if (_tokenAddress == rewardTokens[i]) {
+
+                uint256 earnedByDead = earnedByToken(
+                    _tokenAddress,
+                    DEAD_ADDRESS
+                );
+
+                require(
+                    _recoveryAmount <= earnedByDead,
+                    "DynamicRewardFarm: NOT_ENOUGH_REWARDS"
+                );
+
+                _updateUser(DEAD_ADDRESS);
+
+                rewards[_tokenAddress].userRewards[DEAD_ADDRESS] =
+                rewards[_tokenAddress].userRewards[DEAD_ADDRESS] - _recoveryAmount;
+
+                break;
             }
         }
 
         safeTransfer(
-            _tokenAddress,
+            tokenAddress,
             ownerAddress,
-            _tokenAmount
+            _recoveryAmount
         );
 
         emit Recovered(
-            _tokenAddress,
-            _tokenAmount
+            tokenAddress,
+            _recoveryAmount
         );
     }
 
@@ -527,8 +494,86 @@ contract DynamicRewardFarm is TokenWrapper {
         );
     }
 
+    function _onlyExistingToken(
+        address _tokenAddress
+    )
+        private
+        view
+    {
+        for (uint256 i; i < rewardTokens.length; i++) {
+            if (_tokenAddress == rewardTokens[i]) {
+                return;
+            }
+        }
+
+        revert UnknownToken();
+    }
+
+    /**
+     * @dev Internal function to set reward rate for a token
+     */
+    function _setRewardRate(
+        address _rewardToken,
+        uint256 _newRewardRate
+    )
+        private
+    {
+        _onlyExistingToken(
+            _rewardToken
+        );
+
+        IERC20 tokenAddress = IERC20(
+            _rewardToken
+        );
+
+        RewardData storage r = rewards[
+            _rewardToken
+        ];
+
+        if (block.timestamp < periodFinished) {
+
+            require(
+                r.rewardRate <= _newRewardRate,
+                "DynamicRewardFarm: RATE_CANT_DECREASE"
+            );
+
+            uint256 remainingTime = periodFinished
+                - block.timestamp;
+
+            uint256 remainingRewards = remainingTime
+                * r.rewardRate;
+
+            safeTransfer(
+                tokenAddress,
+                managerAddress,
+                remainingRewards
+            );
+        }
+
+        r.rewardRate = _newRewardRate;
+
+        uint256 newRewardAmount = rewardDuration
+            * _newRewardRate;
+
+        safeTransferFrom(
+            tokenAddress,
+            managerAddress,
+            address(this),
+            newRewardAmount
+        );
+
+        emit RewardAdded(
+            _rewardToken,
+            _newRewardRate,
+            newRewardAmount
+        );
+    }
+
+    /**
+     * @dev Sets the reward rates for multiple reward tokens
+     */
     function setRewardRates(
-        IERC20[] calldata _rewardTokens,
+        address[] calldata _rewardTokens,
         uint256[] calldata _newRewardRates
     )
         external
@@ -536,107 +581,46 @@ contract DynamicRewardFarm is TokenWrapper {
         updateFarm()
     {
         require(
-            _totalStaked > 0,
-            "DynamicRewardFarm: NO_STAKERS"
-        );
-
-        require(
             _rewardTokens.length == _newRewardRates.length,
             "DynamicRewardFarm: ARRAY_LENGTH_MISMATCH"
         );
 
-        uint256 currentPeriodFinish = periodFinished;
+        require(
+            _rewardTokens.length == rewardTokens.length,
+            "DynamicRewardFarm: TOKEN_LENGTH_MISMATCH"
+        );
+
+        _shouldHaveSomeRewards(
+            _newRewardRates
+        );
+
+        for (uint256 i; i < _rewardTokens.length; i++) {
+            _setRewardRate(
+                _rewardTokens[i],
+                _newRewardRates[i]
+            );
+        }
 
         lastUpdateTime = block.timestamp;
         periodFinished = block.timestamp + rewardDuration;
+    }
 
-        if (block.timestamp < currentPeriodFinish) {
-
-            uint256 remainingTime = currentPeriodFinish
-                - block.timestamp;
-
-            for (uint256 i = 0; i < _rewardTokens.length; i++) {
-
-                IERC20 rewardToken = _rewardTokens[i];
-
-                address tokenAddress = address(
-                    rewardToken
-                );
-
-                RewardData storage r = rewards[
-                    tokenAddress
-                ];
-
-                require(
-                    r.rewardRate <= _newRewardRates[i],
-                    "DynamicRewardFarm: RATE_CANT_DECREASE"
-                );
-
-                uint256 rewardRemains = remainingTime
-                    * r.rewardRate;
-
-                safeTransfer(
-                    rewardToken,
-                    managerAddress,
-                    rewardRemains
-                );
-
-                r.rewardRate = _newRewardRates[i];
-
-                uint256 newRewardAmount = rewardDuration
-                    * _newRewardRates[i];
-
-                safeTransferFrom(
-                    rewardToken,
-                    managerAddress,
-                    address(this),
-                    newRewardAmount
-                );
-
-                emit RewardAdded(
-                    tokenAddress,
-                    _newRewardRates[i],
-                    newRewardAmount
-                );
+    /**
+     * @dev Checks if there are any rewards to distribute
+     */
+    function _shouldHaveSomeRewards(
+        uint256[] calldata _newRewardRates
+    )
+        private
+        pure
+    {
+        for (uint256 i; i < _newRewardRates.length; i++) {
+            if (_newRewardRates[i] > 0) {
+                return;
             }
-
-            return;
         }
 
-        uint256 totalRate;
-
-        for (uint256 i = 0; i < _rewardTokens.length; i++) {
-
-            IERC20 rewardToken = _rewardTokens[i];
-
-            address tokenAddress = address(
-                rewardToken
-            );
-
-            totalRate += _newRewardRates[i];
-            rewards[tokenAddress].rewardRate = _newRewardRates[i];
-
-            uint256 newRewardAmount = rewardDuration
-                * _newRewardRates[i];
-
-            safeTransferFrom(
-                rewardToken,
-                managerAddress,
-                address(this),
-                newRewardAmount
-            );
-
-            emit RewardAdded(
-                tokenAddress,
-                _newRewardRates[i],
-                newRewardAmount
-            );
-        }
-
-        require(
-            totalRate > 0,
-            "DynamicRewardFarm: INVALID_RATE"
-        );
+        revert NoRewards();
     }
 
     /**
@@ -686,5 +670,40 @@ contract DynamicRewardFarm is TokenWrapper {
         );
 
         return true;
+    }
+
+    function _updateFarm()
+        private
+    {
+        for (uint256 i; i < rewardTokens.length; i++) {
+            address rewardToken = rewardTokens[i];
+            rewards[rewardToken].perTokenStored = rewardPerToken(
+                rewardToken
+            );
+        }
+
+        lastUpdateTime = lastTimeRewardApplicable();
+    }
+
+    function _updateUser(
+        address _user
+    )
+        private
+    {
+        for (uint256 i; i < rewardTokens.length; i++) {
+
+            address tokenAddress = rewardTokens[i];
+
+            RewardData storage r = rewards[
+                tokenAddress
+            ];
+
+            r.userRewards[_user] = earnedByToken(
+                tokenAddress,
+                _user
+            );
+
+            r.perTokenPaid[_user] = r.perTokenStored;
+        }
     }
 }
